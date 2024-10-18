57d0aa906a0e270c726c949cf6fa70029309fe2a
bljkmp <bljkmp@gmail.com>
2024-10-18 13:20:15 +0000

    More readable

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,8 +1,10 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
+  
   # Which nixpkgs channel to use.
   channel = "unstable"; # or "stable-24.05"
+  
   # Use https://search.nixos.org/packages to find packages
   packages = with pkgs;[
     cargo
@@ -11,25 +13,17 @@
     rustfmt
     stdenv.cc
   ];
+  
   # Sets environment variables in the workspace
   env = {
     RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
   };
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "rust-lang.rust-analyzer"
-      "tamasfe.even-better-toml"
-      "serayuzgur.crates"
-      "vadimcn.vscode-lldb"
-    ];
-    workspace = {
-      onCreate = {
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = ["src/main.rs"];
-      };
-    };
-    # Enable previews and customize configuration
-    previews = {};
-  };
+  
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "rust-lang.rust-analyzer"
+    "tamasfe.even-better-toml"
+    "serayuzgur.crates"
+    "vadimcn.vscode-lldb"
+  ];
 }
